f0cf9dddc46a34ba4f00a35157462e06fba00ece
Claudia Zhang <zjunyi19@gmail.com>
2024-10-04 11:16:28 -0500

    change to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { CardComponent } from "./shared/card/card.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { FormsModule } from "@angular/forms";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 
 
@@ -16,13 +13,10 @@ import { FormsModule } from "@angular/forms";
     declarations: [
         AppComponent, 
         HeaderComponent, 
-        UserComponent, 
-        TasksComponent, 
-        CardComponent, 
-        TaskComponent, 
-        NewTaskComponent],
+        UserComponent
+    ],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule]
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 
 export class AppModule {
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { CardComponent } from "./card/card.component";
+import { NgModule } from "@angular/core";
+
+@NgModule({
+    declarations: [CardComponent],
+    exports: [CardComponent]
+})
+
+export class SharedModule {
+
+}
